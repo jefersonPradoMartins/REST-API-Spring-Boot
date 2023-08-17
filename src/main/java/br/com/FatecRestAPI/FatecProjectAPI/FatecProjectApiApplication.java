d5a518d36a476595724cf774982ed0df31b97dc1
jefersonPradoMartins <jeferson.prado.martins@gmail.com>
2023-08-16 22:11:04 -0300

    update entity

---
+++ src/main/java/br/com/FatecRestAPI/FatecProjectAPI/FatecProjectApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.FatecRestAPI.FatecProjectAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/FatecRestAPI/FatecProjectAPI/FatecProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.FatecRestAPI.FatecProjectAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
