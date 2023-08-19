aeef720784866e7d74b2e62fe5e1fa865651d9ba
jefersonPradoMartins <jeferson.prado.martins@gmail.com>
2023-08-19 14:08:02 -0300

    update

---
+++ src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Controller/CustomerController.java
@@ -0,0 +1,15 @@
+package br.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.FatecRestAPI.FatecProjectAPI.Repository.CustomerRepository;
+
+@RestController
+@RequestMapping (value="/api/v1/customer")
+public class CustomerController {
+
+  
+    
+}
