b13752539a1356b56199661583d009efb2a9e393
jefersonPradoMartins <jeferson.prado.martins@gmail.com>
2023-08-26 15:41:53 -0300

    update final

--- src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Controller/CustomerController.java
+++ src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Controller/CustomerController.java
@@ -1,11 +1,15 @@
 package br.com.FatecRestAPI.FatecProjectAPI.Controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.FatecRestAPI.FatecProjectAPI.Entity.Customer;
 import br.com.FatecRestAPI.FatecProjectAPI.Exception.ResponseGenericException;
 import br.com.FatecRestAPI.FatecProjectAPI.Service.CustomerService;
+
 
 @RestController
 @RequestMapping (value="/api/v1/customer")
@@ -37,6 +42,26 @@ public class CustomerController {
         return  ResponseEntity.status(HttpStatus.CREATED).body(ResponseGenericException.response(result));
     }
 
+    @PostMapping(value = "/update")
+    public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer){
+
+        Customer result = customerService.updateCustomer(customer);
+        return  ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
+    }
+
+    @DeleteMapping(value = "/delete/{idCustomer}")
+    public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer){
+        HashMap<String,Object> result  = customerService.deleteCustomer(idCustomer);
+        return ResponseEntity.status(HttpStatus.OK).body((ResponseGenericException.response(result)));
+
+    }
+
+    @GetMapping(value="/findCustomer/{idCustomer}")
+    public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer) {
+        Optional<Customer> result = customerService.findCustomerById(idCustomer);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
+    }
+    
     
 
 }
--- src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Service/CustomerService.java
+++ src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Service/CustomerService.java
@@ -15,46 +15,67 @@ import br.com.FatecRestAPI.FatecProjectAPI.Repository.CustomerRepository;
 
 @Service
 public class CustomerService {
-    
+
     @Autowired
     private CustomerRepository customerRepository;
 
-    public List<Customer> getInfoCustomer(){
+    public List<Customer> getInfoCustomer() {
 
         return customerRepository.findAll();
     }
 
-    public  Customer saveCustomer(Customer customer){
+    public Customer saveCustomer(Customer customer) {
 
-        if(ValidateCustomer(customer)){
+        if (ValidateCustomer(customer)) {
             return customerRepository.saveAndFlush(customer);
-        }else{
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, 
-                "A renda mensal é obrigatoria e deve ser maior que zero!");
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "A renda mensal é obrigatoria e deve ser maior que zero!");
         }
 
     }
 
-    public Boolean ValidateCustomer(Customer customer){
-        if(customer.getMonthtlyIncomeCustomer()!= null && 
-         customer.getMonthtlyIncomeCustomer().compareTo(BigDecimal.valueOf(0)) >= 0  && 
-         customer.getPasswordCustomer() != null && 
-         !customer.getPasswordCustomer().equals("")){
+    public Boolean ValidateCustomer(Customer customer) {
+        if (customer.getMonthtlyIncomeCustomer() != null &&
+                customer.getMonthtlyIncomeCustomer().compareTo(BigDecimal.valueOf(0)) >= 0 &&
+                customer.getPasswordCustomer() != null &&
+                !customer.getPasswordCustomer().equals("")) {
             return true;
-         }else{
+        } else {
             return false;
-         }
+        }
     }
 
     public HashMap<String,Object> deleteCustomer(Long idCustomer){
         Optional<Customer> customer=  Optional.ofNullable(customerRepository.findById(idCustomer))
-        .orElseThrow(()=> new ResponseStatusException(HttpStatus.NOT_FOUND,
-         "Cliente não encontrado")));
+        .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,
+         "Cliente não encontrado"));
 
          customerRepository.delete(customer.get());
          HashMap<String,Object> result = new HashMap<>();
          result.put("result","Cliente "+customer.get().getFirtNameCustomer()+"" + customer.get().getLastNameCustomer() + "Excluido com sucesso");
          return result;
+    }
+
+    public Optional<Customer> findCustomerById(Long idCustomer){
+        return Optional.ofNullable(customerRepository.findById(idCustomer)
+        .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado")));
+    }
+
+       public Customer updateCustomer(Customer customer) {
+
+        if (ValidateCustomer(customer)) {
+            if(findCustomerById(customer.getIdCustomer())!=null){
+                    return customerRepository.saveAndFlush(customer);
+            }else{
+                return null;
+            }
+            
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "A renda mensal é obrigatoria e deve ser maior que zero!");
+        }
+
     }
 
 }
