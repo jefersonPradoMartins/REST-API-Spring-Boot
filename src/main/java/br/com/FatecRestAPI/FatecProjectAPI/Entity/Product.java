6ea682d433d82b48bae60c675912af7a473f080e
jefersonPradoMartins <jeferson.prado.martins@gmail.com>
2023-08-26 15:47:01 -0300

    update

--- src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Entity/Product.java
+++ src/main/java/br/com/FatecRestAPI/FatecProjectAPI/Entity/Product.java
@@ -1,6 +1,16 @@
 package br.com.FatecRestAPI.FatecProjectAPI.Entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
     
     public Long idProduct ;
+
 }
